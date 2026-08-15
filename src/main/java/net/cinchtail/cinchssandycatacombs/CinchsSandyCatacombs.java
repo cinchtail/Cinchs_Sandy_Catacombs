c29e21ce544239e4d78eb7a12ab4e673b0cf687a
Joshua Davies <cinchtail@gmail.com>
2026-08-15 05:14:54 -0400

    lots of structure work

--- src/main/java/net/cinchtail/cinchssandycatacombs/CinchsSandyCatacombs.java
+++ src/main/java/net/cinchtail/cinchssandycatacombs/CinchsSandyCatacombs.java
@@ -17,3 +17,8 @@ public class CinchsSandyCatacombs implements ModInitializer {
 		ModLootTableModifiers.modifyLootTables();
 	}
 }
+
+//give @s chest[container_loot={loot_table:"cinchssandycatacombs:chests/desert_catacombs_common"}]
+//give @s minecraft:suspicious_gravel[block_entity_data={id:"minecraft:suspicious_sand",LootTable:"cinchssandycatacombs:archaeology/desert_catacombs"}]
+//give @s decorated_pot[block_entity_data={id:"minecraft:decorated_pot",LootTable:"cinchssandycatacombs:pots/desert_catacombs_common"}]
+//give @s decorated_pot[block_entity_data={id:"minecraft:decorated_pot",LootTable:"cinchssandycatacombs:pots/desert_catacombs_rare"},pot_decorations=["minecraft:brick","minecraft:brick","minecraft:brick","minecraft:brewer_pottery_sherd"]]
